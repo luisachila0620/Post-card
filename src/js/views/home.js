7301d22089edb79308ede5afe6c569196758a1f2
luisachila0620 <luisarchila038@gmail.com>
2022-06-02 23:21:01 +0000

    postcard p

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,7 +6,7 @@ export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
 		<p>
-			<img src={rigoImage} />
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9dFpaEVy1Fn0hvEBs7UoiO8fLlyWMaxoIQw&usqp=CAU"/>
 		</p>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
