44e21130d2d9d54f43cebe2ee6f4249b5b5e7294
luisachila0620 <luisarchila038@gmail.com>
2022-06-03 18:03:07 +0000

    added a couple of words

---
+++ src/js/views/postcard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Postcard = () => (
+	<div className="main-title">
+		<h1>Tiger post card!</h1>
+		<div>		<pa className="p1">With only about 2500 tigers in the wild
+			<img className="main-image"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9dFpaEVy1Fn0hvEBs7UoiO8fLlyWMaxoIQw&usqp=CAU"/>
+		</pa>
+		</div>
+
+	</div>
+);
